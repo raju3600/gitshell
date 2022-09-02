e182572a708093c9d8a451ef6b6774168cd6c33b
raju3600 <raju360.n@gmail.com>
2022-09-02 14:17:22 +0530

    this is feature4 commit

--- git-shell.sh
+++ git-shell.sh
@@ -13,34 +13,41 @@ read token
 # Ask for repository name
 echo "Enter repository name"
 read repoName
+
+
 # Check if repository exists at GitHub
 curl -k "https://api.github.com/repos/${username}/${repoName}.git"
-# If repository exits then
 
-if [ $? -eq 0 ]; then
-    cd $repoName
-    # Display unstaged files
-    git status
-    git remote set-url origin https://${token}@github.com/${username}/${repoName}.git
-    # If there are any uncommited and unstatged files, ask user to commit them
+
+echo " find how many branches in the repo "
+a= git branch -v
+echo $a
+
+
+
+	git status
+
+	git remote set-url origin https://${token}@github.com/${username}/{repoName}.git
+
+	 # If there are any uncommited and unstatged files, ask user to commit them
     if [ "$(git status --porcelain)" ]; then
         echo "There are uncommited and unstatged files. Commit them before pushing"
         echo "Enter commit message"
         read commitMessage
         git add .
         git commit -m "$commitMessage"
-        git push
+        git push origin feature4
+
         echo "Files pushed to GitHub"
-        # else push all commited and staged files to remote repo
-    else
+
+	 else
+
         git push
-        echo "Files pushed to GitHub"
-        
+
+        echo "Files nothing pushed to GitHub"
+
     fi
-    #Echo message if there is no files to commit, stage or push
-    if [ "$(git status --porcelain)" ]; then
-        echo "There are no files to commit, stage or push"
-    fi
-else
-    echo "Repository does not exist"
-fi
+
+#echo $b
+
+
